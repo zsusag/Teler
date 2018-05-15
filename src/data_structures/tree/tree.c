85f3ac2bc8acb6b20ea231e43d47eea5d71a48b0
Zachary J. Susag <susagzac@grinnell.edu>
2018-05-14 22:29:29 -0500

    Add functions to traverse a directory tree and print out the contents of the tree to the working directory

--- src/data_structures/tree/tree.c
+++ src/data_structures/tree/tree.c
@@ -1,6 +1,7 @@
 #include <openssl/sha.h>
 #include <stdio.h>
 #include <zlib.h>
+#include <string.h>
 
 #include "tree.h"
 #include "../hash_map/hash_map.h"
@@ -79,4 +80,26 @@ void dirtree_hash(tnode_t* t, hash_map_t* h) {
 // Get the hash of the current root of the directory tree
 char* dirtree_get_hash(tnode_t* t) {
   return t->hash;
+}
+
+void dirtree_reconstruct_working_dir(tnode_t* t, hash_map_t* h, char* path) {
+
+  // Retrieve the information of the current root of the tree from the hash map
+  metadata_t* root_md = hash_map_get(h, t->hash);
+  if(root_md == NULL) {
+    fprintf(stderr, "Unable to find %s in hash map\n", t->hash);
+    exit(EXIT_FAILURE);
+  }
+  // Construct the directory, if it doesn't already exist
+  struct stat root_st = {0};
+  if(stat(path, &root_st) == -1) {
+    // The directory doesn't exist
+    if(mkdir(path, root_md->perm) != 0) {
+      perror("Unable to create directory");
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  // Traverse each child and create the files
+  clist_reconstruct_working_dir(t->children, h, path);
 }
